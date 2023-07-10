56984a4d6069f552c3b00b1d6148f97ad0b4db0f
moonjunsain <moonjunsain@gmail.com>
2023-07-10 16:17:38 -0700

    added convertToGeo function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=6a9214a29813211a9333c8fd3faf05f4
 
 // api url for using longitude and latitude to actually get weather
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=6a9214a29813211a9333c8fd3faf05f4
+// http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=6a9214a29813211a9333c8fd3faf05f4
 
 var searchIn = document.querySelector("#search-input");
 var searchBtn = document.querySelector("#search-btn");
@@ -15,22 +15,39 @@ function getWeather(lat, lon){
         // use queryselectorall to select all classes for each date, emoji, wind, humidity, temp
         // use for loop to change the content to the data retrieved from the server api
         // if its sunny, change the emoji to sunny, rainy then rain etc
-    fetch()
+    var requestUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=5&appid=6a9214a29813211a9333c8fd3faf05f4`
+    fetch(requestUrl)
     .then(function(res){
         return res.json();
     })
     .then(function(data){
-        console.log(data);
+        var weatherDates = document.querySelectorAll(".weather-date");
+
+        console.log("weather data: ", data.list);
     })
 }
 
 function convertToGeo(){
     // triggered when the user clicks on the search btn
+    // gets the value from the user input
+    // adds the value to the search history
     // checks if what user entered was empty or blank space (trim)
     // convert the user entered search input to longitude and latitude by using geo api
     // after getting the right data, call getWeather function with the acquired values => getWeather(lat, lon) 
-    var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname},${statecode},USA&limit=1&appid=6a9214a29813211a9333c8fd3faf05f4`;
-    
+
+    var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${searchIn.value}&limit=1&appid=6a9214a29813211a9333c8fd3faf05f4`;
+    fetch(requestUrl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log("geo data: ", data)
+        var latitude = data[0].lat;
+        var longitude = data[0].lon;
+        getWeather(latitude, longitude);
+    })
+
+
 }
 
 searchBtn.addEventListener("click", convertToGeo)
